7648a1c2911dc6e727470846bececf1a2559c478
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:46:33 +0000

    Only pick user can modify pick

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,4 +1,14 @@
 class PicksController < ApplicationController
+  before_action :current_user_must_be_pick_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_pick_fan
+    pick = Pick.find(params[:id])
+
+    unless current_user == pick.fan
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @picks = Pick.all
 
