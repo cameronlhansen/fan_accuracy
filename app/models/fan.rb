ddbd4508f45930826273b881933ca073079c00f1
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:45:41 +0000

    Validate presence of fans.primary_team_id

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -5,6 +5,8 @@ class Fan < ApplicationRecord
 
   # Validations
 
+  validates :primary_team_id, :presence => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
