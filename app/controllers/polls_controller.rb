75da325dafec01a2a6eda31e22b2d1b6acb4a33a
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:46:33 +0000

    Only poll user can modify poll

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,4 +1,14 @@
 class PollsController < ApplicationController
+  before_action :current_user_must_be_poll_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_poll_fan
+    poll = Poll.find(params[:id])
+
+    unless current_user == poll.fan
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @polls = Poll.all
 
