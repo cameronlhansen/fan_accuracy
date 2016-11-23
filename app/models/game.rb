1ac0390c57de318c257990786c68572349851bda
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:45:41 +0000

    Validate presence of games.home_team_id

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,6 +5,8 @@ class Game < ApplicationRecord
 
   # Validations
 
+  validates :home_team_id, :presence => true
+
   validates :over_under, :numericality => true
 
   validates :spread, :numericality => true
