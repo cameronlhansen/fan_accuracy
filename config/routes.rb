05a93aeb13cf801f715e5d08241d0536d1dc3cb1
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:45:24 +0000

    Generate fans resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :fans
+  # Routes for the Fan resource:
+  # READ
+  get "/fans", :controller => "fans", :action => "index"
+  get "/fans/:id", :controller => "fans", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
