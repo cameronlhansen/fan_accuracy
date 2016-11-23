3883adf342df3a21c4aac4f63d62da108ddc28e8
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:46:08 +0000

    Whitelist columns in Fan for ActiveAdmin

--- app/admin/fan.rb
+++ app/admin/fan.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Fan do
 
+ permit_params :email, :password, :primary_team_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
